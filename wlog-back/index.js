a5d71644b4481dae2242303e32c055f9aa0c972d
Aki Utoslahti <aki.utoslahti@gmail.com>
2018-08-11 12:36:45 +0300

    express initialized

--- wlog-back/index.js
+++ wlog-back/index.js
@@ -1,1 +1,14 @@
-console.log('hello world')
+const express = require('express')
+
+const app = express()
+
+app.get('*', (request, response) => {
+  response.send('Hello world!')
+})
+
+const port = 3000
+const host = 'localhost'
+app.listen(port, () => {
+  console.log(`Server listening on port: ${port}`)
+  console.log(`Access server at: http://${host}:${port}`)
+})
