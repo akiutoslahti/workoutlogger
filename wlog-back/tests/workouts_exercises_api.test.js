bba896ba51a6711b984c8755fb83a7992571ec50
Aki Utoslahti <aki.utoslahti@gmail.com>
2018-08-23 12:09:56 +0300

    added skeletons for more tests

---
+++ wlog-back/tests/workouts_exercises_api.test.js
@@ -0,0 +1,41 @@
+const supertest = require('supertest')
+const { server, app } = require('../index')
+const dbTools = require('./dbTools')
+const db = require('../config/db')
+
+const api = supertest(app)
+const env = require('../config/env')
+
+const baseUrl = '/api/workoutsexercises'
+
+let adminAuth
+let userAuth
+let adminId
+let userId
+
+const newWorkoutExercise = () => ({})
+
+describe('workouts_exercises._api', () => {
+  beforeAll(async () => {
+    await dbTools.initDatabase()
+    await dbTools.adminLogin(api)
+    await dbTools.userLogin(api)
+    ;({ adminAuth, userAuth } = dbTools)
+    const admin = await db.users.find({
+      where: { username: env.ADMIN_USERNAME }
+    })
+    adminId = admin.id
+    const user = await db.users.find({
+      where: { username: env.USER_USERNAME }
+    })
+    userId = user.id
+  })
+  describe(`GET ${baseUrl}`, () => {})
+  describe(`GET ${baseUrl}/:id`, () => {})
+  describe(`POST ${baseUrl}`, () => {})
+  describe(`DELETE ${baseUrl}/:id`, () => {})
+  describe(`PATCH ${baseUrl}/:id`, () => {})
+  afterAll(() => {
+    server.close()
+  })
+})
